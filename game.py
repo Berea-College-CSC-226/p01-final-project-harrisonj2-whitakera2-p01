bb2002f7089b14773adbe54700f9ef461983203a
whitakera2 <whitakera2@berea.edu>
2025-04-21 13:00:18 -0400

    added resized characters and placed character and npc on screen

--- game.py
+++ game.py
@@ -11,7 +11,7 @@
 ####################################################################################
 
 import pygame
-from Npc import NPC
+from Npc import Good_NPC
 from character import Character
 from GUI import MyTkinterApp
 
@@ -26,21 +26,22 @@ class Game:
         self.game_display.blit(self.bg_image, (0, 0))
         pygame.display.update()
         self.player = Character(self.size)
-        #self.npc = NPC(self.size)
+        self.good_npc = Good_NPC(self.size)
 
 
     def run(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
             self.player.movement(pygame.key.get_pressed())
-            #self.npc.movement()
+            self.good_npc.movement()
             self.game_display.blit(self.bg_image, (0, 0))
             self.game_display.blit(self.player.surf, self.player.rect)
-            #self.game_display.blit(self.npc.surf, self.npc.rect)
+            self.game_display.blit(self.good_npc.surf, self.good_npc.rect)
             pygame.display.update()
             self.clock.tick(24)
-            while self.running:
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        self.running = False
+
 
 
 
